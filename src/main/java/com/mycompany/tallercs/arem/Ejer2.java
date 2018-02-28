91c36ddbd004bdc388f32f9def81bcaae9c16ddf
Juan Camilo Mantilla <juan.mantilla-r@mail.escuelaing.edu.co>
2018-02-27 22:04:29 -0500

    Punto 2 y Avance 3

--- src/main/java/com/mycompany/tallercs/arem/Ejer2.java
+++ src/main/java/com/mycompany/tallercs/arem/Ejer2.java
@@ -6,22 +6,38 @@
 package com.mycompany.tallercs.arem;
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 /**
  *
- * @author 2116177
+ * @author Juan Camilo Mantilla
  */
 public class Ejer2 {
-    //FALTA TODO CREO
+    URL pagURL;
     public static void main(String[] args) throws Exception {
-    URL google = new URL("http://www.google.com/");
-    try (BufferedReader reader = new BufferedReader(new InputStreamReader(google.openStream()))) {
-        String inputLine = null;
-        while ((inputLine = reader.readLine()) != null) {
-            System.out.println(inputLine);
+        Scanner pagina = new Scanner(System.in);
+        System.out.print("Ingrese la página deseada: ");
+        String pagStr = pagina.nextLine();
+        try{
+            System.out.println("\n");
+            URL pagURL = new URL(pagStr);
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(pagURL.openStream()))) {
+                String inputLine = null;
+                PrintWriter writer = new PrintWriter("resultado.html");                
+                while ((inputLine = reader.readLine()) != null) {
+                    //System.out.println(inputLine);
+                    writer.println(inputLine);
+                }
+                writer.close();
+                System.out.println("Página generada en el archivo resultado.html");
+            }catch (IOException x) {
+                System.err.println(x);
+            }
         }
-    }catch (IOException x) {
-        System.err.println(x);
-    }
+        catch (MalformedURLException e){
+            System.out.println("Error, URL mal formado");
+        }
+        
+        
     }
 }
